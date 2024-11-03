5d342918cc89c8dc650150145ae3b2830570abd2
gpioblink <gpioblink@gmail.com>
2024-11-04 00:44:32 +0900

    fix: output format

--- usbmsg-read/usbmsg-read.go
+++ usbmsg-read/usbmsg-read.go
@@ -1,42 +1,43 @@
 package main
 
 import (
-    "bufio"
-    "fmt"
-    "os"
-    "regexp"
-    "time"
+	"bufio"
+	"fmt"
+	"os"
+	"regexp"
+	"time"
 )
 
 func watchKmsg(messages chan<- string) {
-    re := regexp.MustCompile(`lun0: file read (\d+) @ (\d+)`)
+	// FIXME: sleepでpollingしない
 
-    for {
-        file, err := os.Open("/dev/kmsg")
-        if err != nil {
-            fmt.Printf("Failed to open /dev/kmsg: %v\n", err)
-            time.Sleep(time.Second) // エラーが発生した場合は1秒待機して再試行
-            continue
-        }
+	re := regexp.MustCompile(`lun0: file read (\d+) @ (\d+)`)
 
-        scanner := bufio.NewScanner(file)
-        for scanner.Scan() {
-            line := scanner.Text()
-            matches := re.FindStringSubmatch(line)
-            if len(matches) == 3 {
-                length := matches[1]
-                address := matches[2]
-                messages <- fmt.Sprintf("READ %s %s", address, length)
-            }
-        }
+	for {
+		file, err := os.Open("/dev/kmsg")
+		if err != nil {
+			fmt.Printf("Failed to open /dev/kmsg: %v\n", err)
+			time.Sleep(time.Second) // エラーが発生した場合は1秒待機して再試行
+			continue
+		}
 
-        if err := scanner.Err(); err != nil {
-            fmt.Printf("Error reading from /dev/kmsg: %v\n", err)
-        }
+		scanner := bufio.NewScanner(file)
+		for scanner.Scan() {
+			line := scanner.Text()
+			matches := re.FindStringSubmatch(line)
+			if len(matches) == 3 {
+				length := matches[1]
+				address := matches[2]
+				messages <- fmt.Sprintf("USBMSG_READ %s %s", address, length)
+			}
+		}
 
-        file.Close()
+		if err := scanner.Err(); err != nil {
+			fmt.Printf("Error reading from /dev/kmsg: %v\n", err)
+		}
 
-        time.Sleep(time.Second)
-    }
+		file.Close()
+
+		time.Sleep(time.Second)
+	}
 }
-
