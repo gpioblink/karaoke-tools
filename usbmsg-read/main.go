b02bdd7e06daa51659d162c9edcd582baf57b8a7
gpioblink <gpioblink@gmail.com>
2024-11-04 07:58:37 +0900

    feat: use fifo

--- usbmsg-read/main.go
+++ usbmsg-read/main.go
@@ -1,12 +1,26 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+)
 
 func main() {
-    messages := make(chan string)
-    go watchKmsg(messages)
-    for msg := range messages {
-        fmt.Println(msg)
-    }
-}
+	messages := make(chan string)
+	go watchKmsg(messages)
 
+	fifo, err := os.OpenFile("/tmp/karaoke-fifo", os.O_WRONLY, 0600)
+	if err != nil {
+		fmt.Println("Error opening FIFO:", err)
+		return
+	}
+	defer fifo.Close()
+
+	for msg := range messages {
+		fmt.Println(msg)
+
+		if _, err := fifo.WriteString(msg + "\n"); err != nil {
+			fmt.Println("Error writing to file:", err)
+		}
+	}
+}
