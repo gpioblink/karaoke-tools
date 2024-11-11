70cf5a187aa3cc3a2bd0761d73fc754fa900177b
gpioblink <gpioblink@gmail.com>
2024-11-11 23:10:32 +0900

    feat: add fat repository

--- karaoke-demon-clean/infrastructure/slot/fat.go
+++ karaoke-demon-clean/infrastructure/slot/fat.go
@@ -1,7 +1,12 @@
 package slot
 
 import (
+	"fmt"
+	"os"
+	"os/exec"
+
 	"gpioblink.com/x/karaoke-demon-clean/domain/reservation"
+	"gpioblink.com/x/karaoke-demon-clean/domain/slot"
 	"gpioblink.com/x/karaoke-demon-clean/domain/video"
 )
 
@@ -18,13 +23,50 @@ import (
 
 type FatRepository struct {
 	memoryRepository *MemoryRepository
+	imagePath        string
+	dummyFilePath    string
 }
 
-func NewFatRepository() *FatRepository {
+const IMAGE_SIZE = "1.8GiB"
+const VIDEO_EXT = "mp4"
+const VIDEO_NUM = "3"
+const VIDEO_SIZE = "512MiB"
+
+func NewFatRepository(imagePath string, dummyFilePath string) (*FatRepository, error) {
+	fmt.Println("Setup image file...")
+
+	// イメージファイルが存在する場合は削除
+	if Exists(imagePath) {
+		os.Remove(imagePath)
+	}
+
+	// makemyfatコマンドにより空イメージファイルの作成
+	// makemyfat create test1.img 2GiB mp4 3 512MiB 1
+	if err := exec.Command("makemyfat", "create",
+		imagePath, IMAGE_SIZE, VIDEO_EXT, VIDEO_NUM, VIDEO_SIZE, "1").Run(); err != nil {
+		// イメージファイルの作成に失敗した場合はエラーを出力
+		fmt.Println("Failed to create image file.")
+		return nil, err
+	}
+
+	fmt.Println("Insert initial video files...")
+
+	// ビデオ数の分だけダミーファイルを書き込み
+	for i := 0; i < 3; i++ {
+		if err := exec.Command("makemyfat", "insert",
+			imagePath, dummyFilePath, fmt.Sprintf("%d", i)).Run(); err != nil {
+			// イメージファイルの追加に失敗した場合はエラーを出力
+			fmt.Println("Failed to insert video.")
+			return nil, err
+		}
+	}
+
 	// Memory実装の拡張として実装する
 	return &FatRepository{
 		memoryRepository: NewMemoryRepository(),
-	}
+		imagePath:        imagePath,
+		dummyFilePath:    dummyFilePath,
+	}, nil
 }
 
 func (f *FatRepository) Len() int {
@@ -32,7 +74,78 @@ func (f *FatRepository) Len() int {
 }
 
 func (f *FatRepository) AttachReservationAndVideoById(slotId int, reservation *reservation.Reservation, video *video.Video) error {
-	// mkmyfatを利用して、
+	// FIXME: 書き込みに失敗した場合のリカバリ処理が必要
 
-	return f.memoryRepository.AttachReservationAndVideoById(slotId, reservation, video)
+	err := f.memoryRepository.AttachReservationAndVideoById(slotId, reservation, video)
+	if err != nil {
+		return err
+	}
+
+	// TODO: 非同期で書き込み処理を行う
+	err = f.writeVideo(slotId, video)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func (f *FatRepository) ChangeVideoById(slotId int, video *video.Video) error {
+	err := f.writeVideo(slotId, video)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func (f *FatRepository) DettachReservationAndVideoById(slotId int) error {
+	return f.memoryRepository.DettachReservationAndVideoById(slotId)
+}
+
+func (f *FatRepository) SetStateById(slotId int, state slot.State) error {
+	return f.memoryRepository.SetStateById(slotId, state)
+}
+
+func (f *FatRepository) SetWritingFlagById(slotId int, isWriting bool) error {
+	return f.memoryRepository.SetWritingFlagById(slotId, isWriting)
+}
+
+func (f *FatRepository) FindById(id int) (*slot.Slot, error) {
+	return f.memoryRepository.FindById(id)
+}
+
+func (f *FatRepository) List() ([]*slot.Slot, error) {
+	return f.memoryRepository.List()
+}
+
+func (f *FatRepository) writeVideo(slotId int, video *video.Video) error {
+	if video == nil {
+		return fmt.Errorf("video is nil")
+	}
+
+	err := f.memoryRepository.SetWritingFlagById(slotId, true)
+	if err != nil {
+		return err
+	}
+
+	// mkmyfatを利用して、ビデオの書き込み作業を行う
+	fmt.Println("Execute:", "makemyfat", "insert", f.imagePath, video.Location(), fmt.Sprintf("%d", slotId))
+	if err := exec.Command("makemyfat", "insert",
+		f.imagePath, video.Location(), fmt.Sprintf("%d", slotId)).Run(); err != nil {
+		// イメージファイルの追加に失敗した場合はエラーを出力
+		fmt.Printf("Failed to insert video %s to fileNo %d.\n", video.Location(), slotId)
+		return err
+	}
+
+	err = f.memoryRepository.SetWritingFlagById(slotId, false)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func Exists(path string) bool {
+	_, err := os.Stat(path)
+	return err == nil
 }
