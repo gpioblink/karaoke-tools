dd32de20d9f6915412cb13aea410d3c72b31c8cb
gpioblink <gpioblink@gmail.com>
2024-11-11 23:17:05 +0900

    chore: remove unused comment

--- karaoke-demon-clean/infrastructure/slot/fat.go
+++ karaoke-demon-clean/infrastructure/slot/fat.go
@@ -10,17 +10,6 @@ import (
 	"gpioblink.com/x/karaoke-demon-clean/domain/video"
 )
 
-// type Repository interface {
-// 	Len() int
-// 	AttachReservationAndVideoById(slotId int, reservation *reservation.Reservation, video *video.Video) error
-// 	ChangeVideoById(slotId int, video *video.Video) error
-// 	DettachReservationAndVideoById(slotId int) error
-// 	SetStateById(slotId int, state State) error
-// 	SetWritingFlagById(slotId int, isWriting bool) error
-// 	FindById(id int) (*Slot, error)
-// 	List() ([]*Slot, error)
-// }
-
 type FatRepository struct {
 	memoryRepository *MemoryRepository
 	imagePath        string
