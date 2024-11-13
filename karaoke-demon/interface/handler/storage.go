02573f44d81e901dfc470a148ced6b4327b65600
gpioblink <gpioblink@gmail.com>
2024-11-13 21:45:16 +0900

    fix addr

--- karaoke-demon/interface/handler/storage.go
+++ karaoke-demon/interface/handler/storage.go
@@ -28,11 +28,11 @@ var UpdateReading HandlerFunc = func(ctx context.Context, service application.Mu
 	// TODO: ハードコードによる決め打ちをなくす
 	// アドレスを元にファイル番号を特定
 	fileIdx := -1
-	if addr >= 0x0000000000502800 && addr < 0x0000000020502800 { // 5253120-542124032
+	if addr >= 0x000000000049c800 && addr < 0x000000002049c800 { // 5253120-542124032
 		fileIdx = 0
-	} else if addr >= 0x0000000020502800+0x1000000 && addr < 0x0000000040502800 {
+	} else if addr >= 0x000000002049c800+0x2000000 && addr < 0x000000004049c800 {
 		fileIdx = 1
-	} else if addr >= 0x0000000040502800+0x1000000 && addr < 0x0000000060502800 {
+	} else if addr >= 0x000000004049c800+0x2000000 && addr < 0x000000006049c800 {
 		fileIdx = 2
 	} else {
 		return
